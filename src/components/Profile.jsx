9734b587bc59d5e08717270703b83a981dc88314
Ahmed Nazir <AhmedNazir@users.noreply.github.com>
2024-10-04 00:36:47 +0600

    init

---
+++ src/components/Profile.jsx
@@ -0,0 +1,11 @@
+export default function Congratulations() {
+  return (
+    <div>
+      <img
+        src="https://dummyimage.com/100x100/000000/fff.jpg&text=Bangladesh"
+        alt="bangladesh"
+      />
+      <p>Bangladesh</p>
+    </div>
+  );
+}
